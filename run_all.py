dabe494a56c125911f9b6e7877fff7ba7bcde7d9
Yizhou Wang <yzwang@dyn-209-2-224-112.dyn.columbia.edu>
2017-05-07 17:57:13 -0400

    update code

--- run_all.py
+++ run_all.py
@@ -1,8 +1,27 @@
 import c3d_utils
 
 
-# Preprocessing
+# Initialization
 
 
+# Read videos
+
+
+# Feature extraction
+
+
+# Dictionary learning
+
+
+# Put sparse features into simple neural networks
+
+
+
+# Put original video into C3D networks
 
 model = c3d_utils.get_model(summary=True)
+
+
+
+# Evaluation
+
