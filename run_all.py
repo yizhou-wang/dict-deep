fc807fa5b141e7b98ad6ea6166420b4f5da9704d
Yizhou Wang <yw2875@columbia.edu>
2017-05-07 22:32:21 -0400

    update code

--- run_all.py
+++ run_all.py
@@ -1,28 +1,52 @@
 import numpy as np
+import ksvd
+from ksvd import ApproximateKSVD
 import mlp_utils
 import c3d_utils
 
 
-# Initialization
+## Initialization ##
 
 
-# Read videos
+## Read videos ##
 
 
-# Feature extraction
+## Feature extraction ##
 
 
-# Dictionary learning
+## Dictionary learning ##
+
+# SAMPLE CODE FOR Dictionary Learning:
+X = Training data 
+n_components = param
+aksvd = ApproximateKSVD(n_components=128)
+dictionary = aksvd.fit(X).components_
+gamma = aksvd.transform(X)
 
 
-# Put sparse features into simple neural networks
+## Put sparse features into simple neural networks ##
+
 model_mlp = mlp_utils.get_model(summary=True)
+model_mlp.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+# Generate dummy data
+data = np.random.random((1000, 100))
+labels = np.random.randint(10, size=(1000, 1))
+
+# Convert labels to categorical one-hot encoding
+one_hot_labels = keras.utils.to_categorical(labels, num_classes=10)
+
+# Train the model, iterating on the data in batches of 32 samples
+model.fit(data, one_hot_labels, epochs=10, batch_size=32)
 
 
-# Put original video into C3D networks
+
+## Put original video into C3D networks ##
 model_c3d = c3d_utils.get_model(summary=True)
 
 
 
-# Evaluation
+## Evaluation ##
 
