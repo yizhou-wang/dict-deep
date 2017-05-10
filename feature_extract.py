ed7b6972ab89a5c2202fff182cbc46ff11163d09
Yizhou Wang <yw2875@columbia.edu>
2017-05-10 18:13:14 -0400

    updata

--- feature_extract.py
+++ feature_extract.py
@@ -5,13 +5,16 @@
 # Compute local motion pattern
 
 
-
 import numpy as np
-import cv2
 from matplotlib import pyplot as plt
+import scipy.io
 from scipy import stats
-# img1 = cv2.imread('box.png',0)          # queryImage
-# img2 = cv2.imread('box_in_scene.png',0) # trainImage
+
+import os
+from os import listdir
+from os.path import isfile, join
+
+import cv2
 
 
 def readvideo(source):
@@ -37,13 +40,13 @@ def readvideo(source):
 		
 		
 		if not retval:
-			#print "Cannot capture frame device"
+			# print "Cannot capture frame device"
 			break
 		
 		#print type(img)
 		#image = rgb2gray(img)
 		imglist.append(img)
-		print '%s %s %s'%(i,'th','frame')
+		# print '%s %s %s'%(i,'th','frame')
 		
 		
 
@@ -60,23 +63,10 @@ def subseq(imglist, N_sq):
 	#print type(chunks[0])
 	return chunks
 
-# def orbkeypoint(img_seq):
-
-# 	# Note by lingyu, for one seq . extract keypoint list
-# 	kp_list_seq = []
-# 	L = len(img_seq) # number of frames in one seq.
-# 	orb = cv2.ORB_create()
-# 	for i in range(0, L):
-# 		current_image = img_seq[i]
-# 		print '%s %s %s'%('original','dimension',current_image.shape)
-# 		kp1, des1 = orb.detectAndCompute(current_image,None)
-# 		# kp1 is list
-# 		kp_list_seq.append(kp1)
-# 	return kp_list_seq
 
 def orbkeypoint(img_frame):
 
-	orb = cv2.ORB_create()
+	orb = cv2.ORB()
 
 	current_image = img_frame
 	print '%s %s %s'%('original','dimension',current_image.shape)
@@ -86,13 +76,6 @@ def orbkeypoint(img_frame):
 	return kp1
 
 
-	# imglist = img
-	# # print type(imglist[0])
-	# #print (imglist[0]).shape[0]
-	# print '%s %s %s'%('original','dimension',(imglist[0]).shape)
-	
-	# #print imglist
-	# # Initiate SIFT detector
 def matching(imglist):
 	orb = cv2.ORB_create()
 	img1 = imglist[0]
@@ -128,22 +111,6 @@ def matching(imglist):
 	plt.imshow(img3),plt.show()
 	
 
-	
-
-# def kplocation(kp_list_seq):
-	
-# 	kploc_seq = []
-# 	# for each frame in the seq
-# 	for f in range(0, len(kp_list_seq)):
-# 		kploc_frame = []
-# 		kp_frame = kp_list_seq[f]
-# 		# for each point in one frame
-# 		for i in range(0, len(kp_frame)):
-# 			kp = kp_frame[i]
-# 			kp_xy = kp.pt
-# 			kploc_frame.append(kp_xy)
-# 		kploc_seq.append(kploc_frame)
-# 	return kploc_seq
 
 def kplocation(kp_frame):
 	
@@ -208,7 +175,7 @@ def motionpattern(kppatch,N_sq):
 	feature = np.reshape(feature,(k,1728))
 	print 'shape'
 	print feature.shape
-	scipy.io.savemat("./feature.mat", feature)
+	
 	return feature
 
 
@@ -222,170 +189,62 @@ def centralmoment(imglist):
 	feature = cv2.moments(image)
 	print feature.shape
 
-# def hog(imglist, seq_id):
-# 	winSize = (128,64)
-# 	blockSize = (16,16)
-# 	blockStride = (8,8)
-# 	cellSize = (8,8)
-# 	nbins = 9
-# 	derivAperture = 0
-# 	winSigma = -1
-# 	histogramNormType = 0
-# 	L2HysThreshold = 2.0000000000000001e-01
-# 	gammaCorrection = 0
-# 	nlevels = 64
-# 	hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,
-#                                         histogramNormType,L2HysThreshold,gammaCorrection,nlevels)
-# 	winStride = (8,8)
-# 	padding = (8,8)
-# 	locations = ((10,20),)
-
-# 	nInterval = 10
-# 	BigCount = 1
-
-# 	index = 1
-
-# 	#index = 0
-
-# 	FirstEntryFlag = False
 
 
-# 	while(index < len(imglist)):
-# 		#print len(imglist)
-# 		hogCount = 0
-# 		for i in range(index,(index + nInterval)):
-			
-# 			# imgPath = pathToVideoFile + "/0 (" + str(i) + ").jpg"
-
-# 			# #READ THE IMAGE HERE
-# 			# img = cv2.imread(imgPath)
-# 			print i
-# 			#print '%s %s %s'%('for',i,'th image')
-# 			img = imglist[i-1]
-# 			#print type(img)
-			
-
-			
-# 			img = cv2.resize(img, (160, 120))
-# 			h1 = hog.compute(img,winStride,padding,locations).T
-# 			#print "Shape of HOG features is: ", h1.shape
-# 			temp = np.copy(h1)
-# 			#print "Shape of temp is: ", temp.shape
-
-# 			if(hogCount == 0):
-# 				hogTemp = np.zeros((nInterval, len(temp[0])))
-#  				#print "Shape of hogTemp is: ", hogTemp.shape
-# 				hogTemp[hogCount]= temp[0]
-# 				if (FirstEntryFlag == False):
-# 				    FirstHOGEntry = np.copy(temp)
-# 				    FirstEntryFlag = True
-# 			else:
-# 				hogTemp[hogCount]= temp
-
-# 			hogCount += 1
-				
-					
-
-# 				#HOGPH = self.computePCA(hogTemp)
-# 		index += nInterval		
-# 		HOGPH = computeHOGPH(hogTemp, FirstHOGEntry)
-		
-			
-
-
-# # print HOGPH
-# 	# print type(HOGPH)
-# 	# np.savetxt('/Users/lingyuzhang/Spring17/4HighDimension/finalproject/ActionLocalization_finalproj/Feature_seq/Descr_' + str(seq_id) + '.mat',HOGPH)
-# 	# print HOGPH.shape
-# 	return HOGPH
-
-# def computeHOGPH(array, firstEntry):
-# 	hogph = firstEntry
-# 	#hogph = np.copy(array[0])
-# 	for j in range(1,len(array)):
-# 	    hogph += array[j-1] - array[j]
-
-# 	return hogph			    
 
 if __name__ == '__main__':
 
-	imglist = readvideo("video.avi")
-	#matching(imglist)
 
+	dataset_name = 'weizmann'
+	root_dir = '../dataset/weizmann/'
+	cat_folders = [f for f in listdir(root_dir) if not isfile(join(root_dir, f))]
+	print cat_folders
 
-
-	N = len(imglist)
-	print '%s %s %s'%('total',N,'frames')
-	N_sq = N
-	video_seq = subseq(imglist, N_sq)
-	print '%s %s %s'%('videoseq',len(video_seq[0]),'frames')
-	print '%s %s %s'%('total',len(video_seq),'segments')
-	# # Whole keypoint list is kp_list
-	# kp_list = []
-	# # for each seq, extract keypoints.
-	# for i in range(0, N_sq):
-	# 	img_seq = video_seq[0][i]
-	# 	kp_list_seq =orbkeypoint(img_seq)
-	# 	kp_list.append(kp_list_seq)
-
-	# kp_frame = orbkeypoint(imglist[0])
-	# kploc_frame = kplocation(kp_frame)
-
-
-
-	# Extract patch for all the subsequence
-	patch = []
-	#video_feature = np.zeros(shape=[])
-	for i in range(0, N/N_sq):
-		sub_video_seq = video_seq[i]
-		kp_frame = orbkeypoint(sub_video_seq[0])
-		kploc_frame = kplocation(kp_frame)
-		kppatch = kppatchfuc(kploc_frame, sub_video_seq)
-		motionpattern(kppatch,N_sq)
+	for cat in cat_folders:
 		
-		# feature = hog(sub_video_seq, i)
-		#np.savetxt('/Users/lingyuzhang/Spring17/4HighDimension/finalproject/ActionLocalization_finalproj/Feature_seq/Descr_' + str(i) + '.mat',feature)
-	
+		cat_dir = root_dir + cat + '/'
+		print cat_dir
 
-		#Extract patch for all the other frames in each subsequence
-		
-	# 	sub_patch_seq = []
+		video_dirs = []
+		for video in os.listdir(cat_dir):
+			print video
+			if video.endswith(".avi"):
+				video_name = video.split('.')[0]
+				cur_video_dir = os.path.join(cat_dir, video)
+				video_dirs.append(cur_video_dir)
 
-	# 	# if i == 0:
-	# 	# 	start = 1
-	# 	# else:
-	# 	# 	start = 0
+				print 'Reading Video ...'
+				imglist = readvideo(cur_video_dir)
+				print video + 'Read!'
+				#matching(imglist)
 
-	# 	for f in range(0, N_sq):
-	# 		img_frame = sub_video_seq[f]
-	# 		patch_frame = kppatch(kploc_frame, img_frame)
-	# 		sub_patch_seq.append(patch_frame)
-		
-	# 	patch.append(sub_patch_seq)
-	# # print len(patch)
-	# # get feature for each subsequence
-	# feature = []
-	# for i in range(0, N/N_sq):
-	# 	video_seq[i]
-	# 	patch_gray_seq = []
-	# 	#print type(patch[i])
-	# 	for f in range(0, len(patch[i])):
-	# 		patch_gray_frame = []
-	# 		for psingle in range(0, len(patch[i][f])):
-	# 			patch_frame_gray = rgb2gray((patch[i])[f][psingle])
-	# 			patch_gray_frame.append(patch_frame_gray)
-	# 		patch_gray_seq.append(patch_gray_frame)
 
-	# 	feature_seq = motionpattern(patch_gray_seq)
+				N = len(imglist)
+				print '%s %s %s'%('total',N,'frames')
+				N_sq = N
+				video_seq = subseq(imglist, N_sq)
+				print '%s %s %s'%('videoseq',len(video_seq[0]),'frames')
+				print '%s %s %s'%('total',len(video_seq),'segments')
 
-	# # hog(imglist)
-	# # 	# print len(sub_video_seq)
-	# # 	feature_seq = motionpattern(patch, sub_video_seq, kp_frame, N_sq, N/N_sq)
-	# # 	feature.append(feature_seq)
-	# # centralmoment(imglist)
-	# # subseq(imglist,2)
-	# # kppatch(0)
 
+				# Extract patch for all the subsequence
+				patch = []
+				#video_feature = np.zeros(shape=[])
+				for i in range(0, N/N_sq):
+					sub_video_seq = video_seq[i]
+					kp_frame = orbkeypoint(sub_video_seq[0])
+					kploc_frame = kplocation(kp_frame)
+					kppatch = kppatchfuc(kploc_frame, sub_video_seq)
+					feature = motionpattern(kppatch,N_sq)
+
+					mat_dir = '../dataset/' + dataset_name + '_features/'
+					if not os.path.exists(mat_dir):
+						os.makedirs(mat_dir)
+					mat_name = mat_dir + video_name + '.mat'
+					scipy.io.savemat(mat_name, {"feature":feature})
+
+					print 'MAT:' + mat_name + 'saved!'
+			
 
 
 
